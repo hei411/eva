675728cf940df8661928554081ed50c83a5871ce
Hei Li <hei411@gmail.com>
2021-10-31 08:49:30 +0000

    comment remover retain code position for better debugging messages from parser

--- src/StringFunctions/CommentHandler.hs
+++ src/StringFunctions/CommentHandler.hs
@@ -4,19 +4,19 @@ commentRemover :: String -> String
 commentRemover str = case str of
   [] -> []
   [c] -> [c]
-  '/' : '/' : tl -> findLn tl
-  '/' : '*' : tl -> findClose tl
+  '/' : '/' : tl -> "  " ++ findLn tl
+  '/' : '*' : tl -> "  " ++ findClose tl
   hd : tl -> hd : (commentRemover tl)
 
 findLn :: String -> String
 findLn str = case str of
   [] -> []
-  '\n' : tl -> commentRemover tl
-  hd : tl -> findLn tl
+  '\n' : tl -> '\n' : commentRemover tl
+  hd : tl -> ' ' : findLn tl
 
 findClose :: String -> String
 findClose str = case str of
   [] -> []
-  [a] -> []
-  '*' : '/' : tl -> commentRemover tl
-  hd : tl -> findClose tl
+  [a] -> [' ']
+  '*' : '/' : tl -> "  " ++ commentRemover tl
+  hd : tl -> if hd == '\n' then '\n' : findClose tl else ' ' : findClose tl
