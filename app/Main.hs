a40ba028539bc63f6e08bd222fec0950593cbef7
Hei Li <hei411@gmail.com>
2021-10-31 11:49:50 +0000

    separated main and parser stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,13 @@
 module Main where
 
-import Parser.MainParser
 import ProgramAnalyzer.ProgramAnalyzer (mainProgramAnalyzer)
-import StringFunctions.CommentHandler
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
   -- Get file name from arguments
   (file_name : _) <- getArgs
-
+  let src_path = ""
   -- Start of real program
-  -- Read from file
-  file_content <- readFile file_name
-  -- Remove comments
-  let clean_file_content = commentRemover file_content
-  -- Create parse tree
-  let parse_tree = mainParser clean_file_content
-  case parse_tree of
-    -- Error in parsing
-    Left parseError -> putStrLn (show (parseError))
-    Right program -> do
-      -- Parsing succeeded
-      putStrLn (show (program))
-      putStrLn ("Program is parsed correctly")
-
-      (compiledFilesData) <- mainProgramAnalyzer file_name program
-      putStrLn ("Program is type-checked correctly.")
-
---putStrLn (show (typeCheckedProgram))
+  compiledFilesData <- mainProgramAnalyzer src_path file_name
+  return ()
--- src/ProgramAnalyzer/ProgramAnalyzer.hs
+++ src/ProgramAnalyzer/ProgramAnalyzer.hs
@@ -3,14 +3,46 @@ module ProgramAnalyzer.ProgramAnalyzer where
 import AscriptionSimplifier.AscriptionSimplifier
 import Datatype
 import ExpTypeConverters.ABExpConverter
+import Parser.MainParser
+import StringFunctions.CommentHandler
+import qualified Text.Parsec as Text.Parsec.Error
 import TypeChecker.MainTypeChecker
 
-mainProgramAnalyzer :: String -> Program -> IO CompiledFilesData
-mainProgramAnalyzer file_name l =
+mainProgramAnalyzer :: String -> String -> IO CompiledFilesData
+mainProgramAnalyzer src_path file_name =
   --need to insert current file name into toCompileFiles
-  mainProgramAnalyzerHelper file_name [] [file_name] [] [] [] [] l
+  mainProgramAnalyzerHelper src_path file_name [] [file_name]
 
 mainProgramAnalyzerHelper ::
+  FilePath ->
+  FilePath ->
+  CompiledFilesData ->
+  [FilePath] ->
+  IO CompiledFilesData
+mainProgramAnalyzerHelper src_path currentFile compiledFilesData toCompileFiles =
+  do
+    parse_tree <- readParse (src_path ++ currentFile)
+    case parse_tree of
+      -- Error in parsing
+      Left parseError -> error (show (parseError))
+      Right program -> do
+        -- Parsing succeeded
+        putStrLn (show (program))
+        putStrLn (currentFile ++ " is parsed correctly")
+        singleFileAnalyzer src_path currentFile compiledFilesData toCompileFiles [] [] [] [] program
+
+readParse :: FilePath -> IO (Either Text.Parsec.Error.ParseError Program)
+readParse fileName =
+  do
+    -- Read from file
+    file_content <- readFile (fileName)
+    -- Remove comments
+    let clean_file_content = commentRemover file_content
+    -- Create parse tree
+    return (mainParser clean_file_content)
+
+singleFileAnalyzer ::
+  FilePath ->
   FilePath ->
   CompiledFilesData ->
   [FilePath] ->
@@ -20,35 +52,45 @@ mainProgramAnalyzerHelper ::
   TypenameList ->
   Program ->
   IO CompiledFilesData
-mainProgramAnalyzerHelper currentFile compiledFilesData toCompileFiles importedFunctions toExportFunctions importedTypenames toExportTypenames l =
-  case l of
-    -- return compiled files and their functions,  type names and functions
-    [] -> return (compiledFilesData)
-    hd : tl -> case hd of
-      LetStatement var parameterList aexp ->
-        do
-          --Step 1: Convert AExp to BExp, i.e. type ascriptions are converted to BTypes (all db indices, even forall)
-          --let bExp = abExpConverter (importedTypenames ++ toExportTypenames) aexp
-          --Step 2: Type check BExp while producing CExp that is substituted with correct previously declared functions
-          -- let (cExp, bType) = mainTypeChecker (importedFunctions ++ toExportFunctions) (TokenlessContext []) bExpSimplified
-          --Step 3: Interpret CExp
-          --Ignore below
-          -- Resolve all type ascriptions
-          --putStrLn (show (cExp))
-          return (compiledFilesData)
-      {-
-        let validTypes = isValidExp (importedTypenames ++ toExportTypenames) exp
-        case validTypes of
-          Right at -> fail (var ++ " has invalid type ascription: " ++ show (at) ++ "!")
-          Left x0 ->
-            do
-              -- Type check exp
-              let t = mainTypeChecker (importedFunctions ++ toExportFunctions) exp
-              case t of
-                Nothing -> fail (var ++ " cannot be type-checked.\n" ++ show (exp))
-                Just checkedType ->
-                  -- TODO: do substitution for predefined functions
-                  mainProgramAnalyzerHelper currentFile compiledFiles toCompileFiles importedFunctions ((var, exp, checkedType) : toExportFunctions) importedTypenames toExportTypenames tl
-      -}
-      -- For future alternative statements
-      _ -> mainProgramAnalyzerHelper currentFile compiledFilesData toCompileFiles importedFunctions toExportFunctions importedTypenames toExportTypenames tl
+singleFileAnalyzer src_path currentFile compiledFilesData toCompileFiles importedFunctions toExportFunctions importedTypenames toExportTypenames program = case program of
+  [] ->
+    do
+      putStrLn (src_path ++ currentFile ++ " completely typechecked.")
+      return ((src_path ++ currentFile, toExportFunctions, toExportTypenames) : compiledFilesData)
+  hd : tl -> case hd of
+    LetStatement str polyParams aExp -> error "LetStatement processor not implemented"
+    TypeStatement str typeParams aType -> error "TypeStatement processor not implemented"
+    ImportStatement fileName -> error "ImportStatement processor not implemented"
+
+{-case l of
+          -- return compiled files and their functions,  type names and functions
+          [] -> return (compiledFilesData)
+          hd : tl -> case hd of
+            LetStatement var parameterList aexp ->
+              do
+                --Step 1: Convert AExp to BExp, i.e. type ascriptions are converted to BTypes (all db indices, even forall)
+                --let bExp = abExpConverter (importedTypenames ++ toExportTypenames) aexp
+                --Step 2: Type check BExp while producing CExp that is substituted with correct previously declared functions
+                -- let (cExp, bType) = mainTypeChecker (importedFunctions ++ toExportFunctions) (TokenlessContext []) bExpSimplified
+                --Step 3: Interpret CExp
+                --Ignore below
+                -- Resolve all type ascriptions
+                --putStrLn (show (cExp))
+                return (compiledFilesData)
+            {-
+              let validTypes = isValidExp (importedTypenames ++ toExportTypenames) exp
+              case validTypes of
+                Right at -> fail (var ++ " has invalid type ascription: " ++ show (at) ++ "!")
+                Left x0 ->
+                  do
+                    -- Type check exp
+                    let t = mainTypeChecker (importedFunctions ++ toExportFunctions) exp
+                    case t of
+                      Nothing -> fail (var ++ " cannot be type-checked.\n" ++ show (exp))
+                      Just checkedType ->
+                        -- TODO: do substitution for predefined functions
+                        mainProgramAnalyzerHelper currentFile compiledFiles toCompileFiles importedFunctions ((var, exp, checkedType) : toExportFunctions) importedTypenames toExportTypenames tl
+            -}
+            -- For future alternative statements
+            _ -> mainProgramAnalyzerHelper currentFile compiledFilesData toCompileFiles importedFunctions toExportFunctions importedTypenames toExportTypenames tl
+-}
